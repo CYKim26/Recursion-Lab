4ade3b53f88b9d5c7729c529b4a9ab0ebd26a10c
CYKim26 <caden.kim26@avenues.org>
2024-12-15 18:29:19 -0500

    Update circles.js

--- circles.js
+++ circles.js
@@ -1,1 +1,31 @@
+function setup() {
+  createCanvas(400, 400)
+  rectMode(CENTER)
+  noFill()
+  noLoop()
+}
 
+function draw() {
+  background(220)
+  rSquare(width/4)
+  rSquare(width/4, 3*width/4, width/4)
+}
+
+function rSquare(s) {
+  if( s > 0 ) { // base case
+    rect(width/2, height/2, s, s)
+    rSquare(s/2)
+  }
+}
+
+function rSquare(x1,x2,s) {
+  const d = abs(x1-x2)
+  if( d > 4 ) { // base case
+    const halfSize = s/2
+    rect(x1,height/2,s,s)
+    
+    rect(x2,height/2,s,s)
+    rSquare(x1-halfSize,x1+halfSize, s/2)
+    rSquare(x2-halfSize,x2+halfSize, s/2)
+  }
+}
